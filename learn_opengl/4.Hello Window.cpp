49ae21d61d868e089606c55e8afbdcb0eddc6946
Joe <695993410@qq.com>
2020-03-31 12:08:47 +0800

    完成4.Hello Window

---
+++ learn_opengl/4.Hello Window.cpp
@@ -0,0 +1,105 @@
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
+#include<iostream>
+using namespace std;
+void frame_buffer_size_callback(GLFWwindow *window, int width, int height);
+void preocessInput(GLFWwindow *window);
+
+int main() {
+	/*
+	 * 1. GLFW初始化
+	 */
+
+	glfwInit();//初始化GLFW
+
+	//配置GLFW。参数1：想要配置的选项，参数2：选项值
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//使用核心模式
+
+	//创建窗口
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();//完成，销毁所有的窗口、cursor
+
+		return -1;
+	}
+
+	//创建上下文
+	glfwMakeContextCurrent(window);
+
+
+
+	/*
+	 * 2. GLAD初始化
+	 */
+
+	 //在调用OpenGL函数前需要对GLAD初始化
+	 //类型转换是啥意思？
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to initialize GLAD" << endl;
+		return -1;
+	}
+
+	/*
+	 * 3. Viewport
+	 */
+	glViewport(0, 0, 800, 600);
+
+	//监听窗口调整的回调函数
+	glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+
+	/*
+	 * 4. render loop
+	 */
+
+	 //使得窗口不会马上关闭
+	while (!glfwWindowShouldClose(window))//检测此窗口是否被要求关闭
+	{
+		preocessInput(window);//处理输入
+
+		/*
+		 * 7. 设置clear color及clear
+		 */
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		//放置渲染的指令代码
+		//...
+
+
+		glfwSwapBuffers(window);//交换color buffer
+		glfwPollEvents();//监听是否有任何事件被触发，比如键盘输入，鼠标移动，更新窗口状态，并调用对应的函数
+	}
+
+	/*
+	 * 5. 退出了render loop，销毁资源
+	 */
+	glfwTerminate();//销毁分配的内存空间
+
+
+
+	return 0;
+}
+//回调函数
+void frame_buffer_size_callback(GLFWwindow *window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+/*
+ * 6. 处理输入
+ */
+void preocessInput(GLFWwindow *window) {
+	/*
+	 * 如果输入的是Esc键，则退出循环，关闭窗口;
+	 * 如果不是的话，glfwGetKey会返回GLFW_RELEASE
+	 */
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
